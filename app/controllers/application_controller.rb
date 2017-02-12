47740976de35bd3c07b9b8434b56e1379c014c40
mmelmer <mikemelmer@gmail.com>
2017-02-12 13:17:13 -0500

    created /login path in app controller and created login.erb page, with post info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,23 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user !=nil && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/signup' do
+    erb :'/users/create'
+  end
+
+
 end
