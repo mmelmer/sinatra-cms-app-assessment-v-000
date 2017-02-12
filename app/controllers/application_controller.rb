a7f4d1dbd5626659016c9f210a14ab3828c6317c
mmelmer <mikemelmer@gmail.com>
2017-02-12 13:46:52 -0500

    added user create page and corresponding get/post paths in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,5 +31,16 @@ class ApplicationController < Sinatra::Base
     erb :'/users/create'
   end
 
+  post '/signup' do
+    if (params[:username] == "") || (params[:email] == "") || (params[:password] == "")
+      redirect '/signup'
+    else
+      @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect '/'
+    end
+  end
+
 
 end
